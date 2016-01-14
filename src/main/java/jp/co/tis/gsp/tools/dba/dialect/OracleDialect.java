c49cbf0dd10fd77646005dc4d6618155344fb3b0
tamura.naoki <tamura.naoki@tis.co.jp>
2016-01-14 11:55:40 +0900

    Oracle12c対応。Exportで必要となる権限を付与。
    CONNECT, RESOURCEロールを権限レベルに分解。

--- src/main/java/jp/co/tis/gsp/tools/dba/dialect/OracleDialect.java
+++ src/main/java/jp/co/tis/gsp/tools/dba/dialect/OracleDialect.java
@@ -198,7 +198,9 @@ public class OracleDialect extends Dialect {
 				// DROP USERに失敗しても気にしない
 			}
 			stmt.execute("CREATE USER "+ user + " IDENTIFIED BY "+ password + " DEFAULT TABLESPACE users");
-            String grantSql = "GRANT CONNECT, RESOURCE, SELECT ANY TABLE, CREATE VIEW, CREATE ANY TABLE, CREATE SYNONYM, CREATE ANY DIRECTORY TO " + user;
+			String grantSql = "GRANT CREATE SESSION, UNLIMITED TABLESPACE, CREATE CLUSTER, CREATE INDEXTYPE, CREATE OPERATOR, " + 
+	                   "CREATE PROCEDURE, CREATE SEQUENCE, CREATE TABLE, CREATE TRIGGER, CREATE TYPE, SELECT ANY TABLE, " + 
+	                   "CREATE VIEW, CREATE ANY TABLE, CREATE SYNONYM, CREATE ANY DIRECTORY TO " + user;
 			stmt.execute(grantSql);
             System.err.println(grantSql);
             String grantExecuteSql = "GRANT EXECUTE ON SYS.DBMS_ALERT TO " + user;
