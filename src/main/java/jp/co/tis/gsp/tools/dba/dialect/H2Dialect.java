ab388c422a2301bfa4b0281d6485e58843660bc2
blackawa <3104kurokawa@gmail.com>
2016-02-01 16:50:35 +0900

    H2Databaseで、ユーザ名とスキーマ名が不一致でも動作するようにした。
    - 元のデータベースをdropする時管理者ユーザで実行するようにした。
    - 実装されていなかったcreateUseを実装した。
    - ユーザ名とスキーマ名が異なる時でも動作するよう、スキーマ作成や権限追加を行った。
    - generate-entityが成功するようにadminPasswordをdiconに渡す時NULLを空文字として渡すようにした。

--- src/main/java/jp/co/tis/gsp/tools/dba/dialect/H2Dialect.java
+++ src/main/java/jp/co/tis/gsp/tools/dba/dialect/H2Dialect.java
@@ -1,18 +1,14 @@
 package jp.co.tis.gsp.tools.dba.dialect;
 
-import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import jp.co.tis.gsp.tools.db.TypeMapper;
-
 import org.apache.maven.plugin.MojoExecutionException;
 import org.seasar.extension.jdbc.gen.dialect.GenDialectRegistry;
 import org.seasar.extension.jdbc.util.ConnectionUtil;
 import org.seasar.framework.util.DriverManagerUtil;
 import org.seasar.framework.util.StatementUtil;
+
+import java.io.File;
+import java.sql.*;
 
 public class H2Dialect extends Dialect {
     private String url;
@@ -52,7 +48,7 @@ public class H2Dialect extends Dialect {
         Connection conn = null;
         Statement stmt = null;
         try {
-            conn = DriverManager.getConnection(url, user, password);
+            conn = DriverManager.getConnection(url, adminUser, adminPassword);
             stmt = conn.createStatement();
             stmt.execute("DROP ALL OBJECTS");
         } catch (SQLException e) {
@@ -83,18 +79,47 @@ public class H2Dialect extends Dialect {
     @Override
     public void createUser(String user, String password, String adminUser,
             String adminPassword) throws MojoExecutionException {
-        // TODO 自動生成されたメソッド・スタブ
-
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = DriverManager.getConnection(url, adminUser, adminPassword);
+            stmt = conn.createStatement();
+            stmt.execute("CREATE USER " + user + " PASSWORD '" + password + "'");
+//            stmt.execute("ALTER USER " + user + " ADMIN TRUE");
+        } catch (SQLException e) {
+            throw new MojoExecutionException("CREATE USER 実行中にエラー: ", e);
+        } finally {
+            StatementUtil.close(stmt);
+            ConnectionUtil.close(conn);
+        }
     }
 
     @Override
-    public void grantAllToAnotherSchema(Connection conn, String schema, String user) {
-        throw new UnsupportedOperationException("このデータベースで実行する時は、別スキーマは指定できません。");
+    public void grantAllToAnotherSchema(Connection conn, String schema, String user) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA=?");
+        pstmt.setString(1, schema);
+        ResultSet rs = pstmt.executeQuery();
+        StringBuilder sb = new StringBuilder();
+        while (rs.next()) {
+            sb.append(schema).append(".").append(rs.getString("TABLE_NAME")).append(",");
+        }
+        int stringLength = sb.length();
+        // 最後尾にもカンマがついてしまうので手動で消す
+        String tables = sb.toString().substring(0, stringLength - 1);
+
+        // PUBLICスキーマはデフォルトスキーマなので何もする必要がない
+        if ("PUBLIC".equals(schema)) return;
+        Statement stmt = conn.createStatement();
+
+        stmt.execute("GRANT ALL ON " + tables + " TO " + user);
+        StatementUtil.close(stmt);
     }
 
     @Override
-    public void createSchemaIfNotExist(Connection conn, String schema) {
-        throw new UnsupportedOperationException("このデータベースで実行する時は、別スキーマは指定できません。");
+    public void createSchemaIfNotExist(Connection conn, String schema) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE SCHEMA IF NOT EXISTS " + schema);
+        StatementUtil.close(stmt);
     }
 
     @Override
