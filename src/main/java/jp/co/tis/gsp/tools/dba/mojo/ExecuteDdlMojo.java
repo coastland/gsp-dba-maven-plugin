b472afb57d3773b6ba9a16b3137ddd58beb21fc2
tamura.naoki <tamura.naoki@tis.co.jp>
2016-04-07 21:09:42 +0900

    onErrorパラメータの追加。execute-ddlとload-dataの時にエラー発生時の挙動を指定出来るようにする。

--- src/main/java/jp/co/tis/gsp/tools/dba/mojo/ExecuteDdlMojo.java
+++ src/main/java/jp/co/tis/gsp/tools/dba/mojo/ExecuteDdlMojo.java
@@ -97,10 +97,11 @@ public class ExecuteDdlMojo extends AbstractDbaMojo {
                 return f1.getName().compareTo(f2.getName());
             }
         });
+
 		try {
-            executeBySqlFiles(files.toArray(new File[files.size()]));
+			executeBySqlFiles(files.toArray(new File[files.size()]));
 		} catch (Exception e) {
-			getLog().warn(e);
+			getLog().error(e);
 			throw new MojoExecutionException("SQL実行中にエラーが発生しました:", e);
 		}
 
@@ -150,7 +151,7 @@ public class ExecuteDdlMojo extends AbstractDbaMojo {
 
     }
 
-    private void executeBySqlFiles(File...sqlFiles) throws SQLException, IOException {
+    private void executeBySqlFiles(File...sqlFiles) throws SQLException, IOException, MojoExecutionException {
         if (conn == null || conn.isClosed()) {
             setConnection();
         }
@@ -162,6 +163,12 @@ public class ExecuteDdlMojo extends AbstractDbaMojo {
             try {
                 reader = new FileReader(sqlFile);
                 runStatements(reader);
+            } catch(Exception e){
+            	getLog().warn(e);
+            	
+            	if(onError.equals("abort"))
+            	  throw new MojoExecutionException("SQL実行中にエラーが発生しました:", e);
+            	
             } finally {
                 IOUtils.closeQuietly(reader);
             }
