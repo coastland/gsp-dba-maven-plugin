705f9914d890ef66d0ea7d62531e40543b990615
tamura.naoki <tamura.naoki@tis.co.jp>
2016-04-15 15:23:01 +0900

    _USEQ削除作業の対応漏れ。シーケンスに関するエンティティへのgenerationTypeの設定などはs2jdbcに任せる。

--- src/main/java/jp/co/tis/gsp/tools/dba/s2jdbc/gen/GspAttributeDescFactoryImpl.java
+++ src/main/java/jp/co/tis/gsp/tools/dba/s2jdbc/gen/GspAttributeDescFactoryImpl.java
@@ -16,6 +16,8 @@
 
 package jp.co.tis.gsp.tools.dba.s2jdbc.gen;
 
+import javax.persistence.GenerationType;
+
 import org.seasar.extension.jdbc.gen.desc.AttributeDesc;
 import org.seasar.extension.jdbc.gen.dialect.GenDialect;
 import org.seasar.extension.jdbc.gen.internal.desc.AttributeDescFactoryImpl;
@@ -23,9 +25,6 @@ import org.seasar.extension.jdbc.gen.meta.DbColumnMeta;
 import org.seasar.extension.jdbc.gen.meta.DbForeignKeyMeta;
 import org.seasar.extension.jdbc.gen.meta.DbTableMeta;
 import org.seasar.framework.convention.PersistenceConvention;
-import org.seasar.framework.util.ClassUtil;
-
-import javax.persistence.GenerationType;
 
 /**
  * @author kawasima
@@ -42,13 +41,6 @@ public class GspAttributeDescFactoryImpl extends AttributeDescFactoryImpl {
     protected void doGenerationType(DbTableMeta tableMeta,
                                     DbColumnMeta columnMeta, AttributeDesc attributeDesc) {
         super.doGenerationType(tableMeta, columnMeta, attributeDesc);
-
-        // オートインクリメントが付いているものはgenerationTypeを設定する。
-        if (columnMeta.isAutoIncrement() && attributeDesc.getGenerationType() == null) {
-            attributeDesc.setGenerationType(generationType);
-            attributeDesc.setInitialValue(initialValue);
-            attributeDesc.setAllocationSize(allocationSize);
-        }
 
         // PrimaryKeyでも外部キーになっているものは、ID生成しないので対象から外す。
         if (attributeDesc.getGenerationType() != null) {
