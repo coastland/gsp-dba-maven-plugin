6e969b03f7daa9d17205e13fe424e9c77209460f
tamutamu <tamu.0.0.tamu@gmail.com>
2016-05-10 15:26:39 +0900

    Java6実行対応 (#95)
    * Postgresql:Import時のファイル入力を標準入力から行うのではなく、-fオプションでファイルを指定するように変更。
    
    * JPAテスト:Java6実行対応

--- src/main/java/jp/co/tis/gsp/tools/dba/dialect/PostgresqlDialect.java
+++ src/main/java/jp/co/tis/gsp/tools/dba/dialect/PostgresqlDialect.java
@@ -17,9 +17,12 @@
 package jp.co.tis.gsp.tools.dba.dialect;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -41,7 +44,6 @@ import org.seasar.framework.util.ResultSetUtil;
 import org.seasar.framework.util.StatementUtil;
 
 import jp.co.tis.gsp.tools.db.TypeMapper;
-import jp.co.tis.gsp.tools.dba.util.ProcessUtil;
 
 public class PostgresqlDialect extends Dialect {
     private static final List<String> USABLE_TYPE_NAMES = new ArrayList<String>();
@@ -178,17 +180,43 @@ public class PostgresqlDialect extends Dialect {
         if (StringUtils.isNotEmpty(password)) {
             environment.put("PGPASSWORD", password);
         }
+        
+        Process process = null;
+        InputStream stdout = null;
+        BufferedReader br = null;
+        
         try {
-            ProcessUtil.execWithInput(dumpFile,
-                    environment,
+            String[] args = new String[]{
                     "psql",
                     "--host", getHost(),
                     "--port", getPort(),
                     "--username", user,
-                    getDatabase()
-            );
+                    "-f", dumpFile.getAbsolutePath(),
+                    getDatabase()};
+            
+            ProcessBuilder pb = new ProcessBuilder(args);
+            pb.redirectErrorStream(true);
+            if (environment != null) {
+                pb.environment().putAll(environment);
+            }
+            
+            process = pb.start();
+            stdout = process.getInputStream();
+            br = new BufferedReader(new InputStreamReader(stdout));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            
         } catch (Exception e) {
             throw new MojoExecutionException("スキーマインポート実行中にエラー", e);
+        } finally {
+            IOUtils.closeQuietly(br);
+            IOUtils.closeQuietly(stdout);
+            
+            if(process != null){
+            	process.destroy();
+            }
         }
     }
 
